9cd60c00c4100252c1f5e41b01d3ef960cbcd47b
Christian Kornblum <kornblum@adesso-mobile.de>
2023-10-11 14:15:58 +0200

    initial project

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    $schema: 'http://json.schemastore.org/prettierrc',
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    semi: true,
+    htmlWhitespaceSensitivity: 'css',
+    printWidth: 120,
+    quoteProps: 'as-needed',
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'es5',
+    useTabs: false,
+    proseWrap: 'always',
+};
